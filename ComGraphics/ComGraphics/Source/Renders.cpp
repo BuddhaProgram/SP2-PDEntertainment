976369fbf27cffbb0527ef6e8dde92eea1c66d78
teikiyohira <qingping1998@gmail.com>
2016-02-19 15:18:09 +0800

    Top quadrons of map loaded.

--- ComGraphics/ComGraphics/Source/Renders.cpp
+++ ComGraphics/ComGraphics/Source/Renders.cpp
@@ -192,9 +192,60 @@ void SPGame::RenderLevel1()
     
 
 
-    
-  
+    //------------------------------------------------------------------------
+	//top quads
 
+	RenderRightWall(63, 47, -43);//1
+	RenderDownWall(-10, -43, 62);//2
+	RenderLeftWall(63, 47, -10);//3
+	RenderUpWall(-10, -35, 47);//4
+	RenderUpWall(-38, -43, 47);//5
+
+	RenderRightWall(47, 38, -38); //6
+	RenderLeftWall(47, 41, -35); //7
+	RenderDownWall(0, -35, 41);//8
+	RenderUpWall(-20, -38, 38); //9
+	RenderRightWall(38, 28, -20);//10
+	RenderLeftWall(38, 28, -17); //11
+	RenderUpWall(3, -17, 38); //12
+
+	RenderLeftWall(51, 38, 3); //13
+	RenderRightWall(51, 41, 0); //14
+	RenderUpWall(0, -4, 51);//15
+	RenderUpWall(10, 3, 51); //16
+	RenderLeftWall(62, 51, 10); //17
+	RenderRightWall(62, 51, -4);//18
+	RenderDownWall(10, -4, 62); //19
+
+	RenderDownWall(-20, -28, 28);//20
+	RenderDownWall(-5, -17, 28);//21
+	RenderRightWall(28, 10, -28);//22
+	RenderUpWall(-5, -28, 10);//23
+	RenderLeftWall(15, 10, -5);//24
+	RenderLeftWall(28, 18, -5);//25
+
+	RenderDownWall(10, -5, 18);//26
+	RenderUpWall(10, -5, 15);//27
+	RenderRightWall(25, 18, 10);//28
+	RenderRightWall(15, 7, 10);//29
+	RenderUpWall(24, 10, 7);//30
+	RenderDownWall(27, 10, 25);//31
+
+	RenderDownWall(40, 30, 25);//32
+	RenderLeftWall(25, 7, 40);//33
+	RenderUpWall(40, 27, 7);//34
+	RenderRightWall(31, 25, 27);//35
+	RenderLeftWall(31, 25, 30);//36
+
+	RenderUpWall(27, 20, 31);//37
+	RenderUpWall(35, 30, 31);//38
+	RenderRightWall(35, 31, 20);//39
+	RenderLeftWall(35, 31, 35);//40
+	RenderDownWall(35, 20, 35);//41
+
+
+  //TOP QUAD END
+	//-----------------------------------------------------
 
 
 	/*----------------------------------[Renders Facility Walls and Ceilings]------------------------------------*/
