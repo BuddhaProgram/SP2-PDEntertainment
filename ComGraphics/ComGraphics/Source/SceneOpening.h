f76516a039dde743a00ab99d61a77fd5ad357fbb
BuddhaProgram <ng_junguo901@hotmail.com>
2016-02-24 22:40:22 +0800

    Changlog:
    1. Added a main menu to the game.
    - There are two options: to start the game or exit the game.

---
+++ ComGraphics/ComGraphics/Source/SceneOpening.h
@@ -0,0 +1,94 @@
+#ifndef SCENEOPENING_H
+#define SCENEOPENING_H
+
+#include "Scene.h"
+#include "Camera3.h"
+#include "Vector3.h"
+#include "Mesh.h"
+#include "Misc.h"
+#include "MatrixStack.h"
+#include "Light.h"
+#include "HostileMob.h"
+#include "MobGhost.h"
+
+class SceneOpening : public Scene
+{
+	enum GEOMETRY_TYPE
+	{
+		GEO_LIGHTBALL,
+		GEO_TEXT,
+
+		NUM_GEOMETRY,
+	};
+	enum UNIFORM_TYPE
+	{
+		U_MVP = 0,
+		U_MODELVIEW,
+		U_MODELVIEW_INVERSE_TRANSPOSE,
+		U_MATERIAL_AMBIENT,
+		U_MATERIAL_DIFFUSE,
+		U_MATERIAL_SPECULAR,
+		U_MATERIAL_SHININESS,
+
+		U_LIGHT0_POSITION,
+		U_LIGHT0_COLOR,
+		U_LIGHT0_POWER,
+		U_LIGHT0_KC,
+		U_LIGHT0_KL,
+		U_LIGHT0_KQ,
+		U_LIGHTENABLED,
+		U_LIGHT0_TYPE,
+		U_LIGHT0_SPOTDIRECTION,
+		U_LIGHT0_COSCUTOFF,
+		U_LIGHT0_COSINNER,
+		U_LIGHT0_EXPONENT,
+
+		U_NUMLIGHTS,
+		U_COLOR_TEXTURE_ENABLED,
+		U_COLOR_TEXTURE,
+		U_TEXT_ENABLED,
+		U_TEXT_COLOR,
+		U_TOTAL,
+	};
+public:
+	SceneOpening();
+	~SceneOpening();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Reset();
+	virtual void Exit();
+
+	Camera3 camera;
+private:
+	void RenderMesh(Mesh *mesh, bool enableLight);
+
+	//render functions 
+	void RenderText(Mesh* mesh, std::string text, Color color);
+	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
+	void RenderModelOnScreen(Mesh* mesh, float Sx, float Sy, float Sz, float Rotate, float rX, float rY, float rZ, float x, float y, float z, bool LightYN);
+
+	unsigned m_vertexArrayID;
+	Mesh *meshList[NUM_GEOMETRY];
+
+	unsigned m_programID;
+	unsigned m_parameters[U_TOTAL];
+
+	MS modelStack, viewStack, projectionStack;
+	
+	// Change Color
+	unsigned int ui_ChooseOption;
+	float ChooseOptionDebounce;
+	void OptionChange(double dt);
+	void RenderOptionChange();
+
+	void ChangeScene();
+
+	Application App;
+
+	Light light[1];
+
+};
+
+#endif
