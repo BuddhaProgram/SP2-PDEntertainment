da1767e80be9559e30cf4f72758e2b86665bc963
RoseHime <blackdarkrose91@gmail.com>
2016-02-29 15:57:40 +0800

    Just changing Scene = End to Scene = Opening

--- ComGraphics/ComGraphics/Source/Application.cpp
+++ ComGraphics/ComGraphics/Source/Application.cpp
@@ -158,7 +158,7 @@ void Application::Run()
 	//SceneStart *scene = new SceneStart();
 	//scene->Init();
 
-	scene = End;
+	scene = Opening;
 
 
 	m_timer.startTimer();    // Start timer to calculate how long it takes to render this frame
